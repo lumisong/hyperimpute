1ab60afd81ab5f8747d1e12c932f6004e0691a9d
lumisong <1101474381@qq.com>
2024-03-21 19:45:02 +0800

    打包分析

--- src/hyperimpute/plugins/imputers/plugin_hyperimpute.py
+++ src/hyperimpute/plugins/imputers/plugin_hyperimpute.py
@@ -140,3 +140,9 @@ class HyperImputePlugin(base.ImputerPlugin):
 plugin = HyperImputePlugin
 
 
+
+
+import numpy as np
+from hyperimpute.plugins.imputers import Imputers
+plugin = Imputers().get("hyperimpute")
+plugin.fit_transform([[1, 1, 1, 1], [np.nan, np.nan, np.nan, np.nan], [1, 2, 2, 1], [2, 2, 2, 2]])
